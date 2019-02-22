8ae55a3383956d828c49e2f53e1a82514000ec31
Vibergf <vibergf@student.chalmers.se>
2019-02-22 17:06:12 +0100

    Added check to skip matches that are already downloaded.

--- gggreplays/getter.py
+++ gggreplays/getter.py
@@ -1,11 +1,14 @@
 
+import os
 import requests
 import pprint
 import json
 
-pp = pprint.PrettyPrinter(indent=4)
 limitParam = 10 #There are ~4200 matches of Platinum, ~2500 matches of Diamond, ~500 matches of Master
 leagueParam = 4 #3 for Platinum, 4 for Diamond, 5 for Master
+targetPath = "../../gggreplays/" #Target path for replay files. Make sure the folder exists!
+
+pp = pprint.PrettyPrinter(indent=4)
 url = "https://gggreplays.com/api/v1/matches?average_league=" + str(leagueParam) + "&game_type=1v1&replay=true&vs_race=protoss&limit=" + str(limitParam)
 
 
@@ -19,11 +22,12 @@ listLength = len(myJson["collection"])
 
 for x in range(0, listLength):
     id = myJson["collection"][x]["id"]
+    if os.path.isfile(targetPath + str(id) + ".SC2Replay"):
+        continue
     urlDl = "https://gggreplays.com/matches/" + str(id) + "/replay"
     print("Downloading replay ", urlDl, ", ", str(x), "of", listLength, " :: ", round((x/listLength * 100), 2), "%")
     r = requests.get(urlDl, allow_redirects=True)
-    # Skapa denna mappen!
-    open("../../gggreplays/" + str(id) + ".SC2Replay", "wb").write(r.content)
+    open(targetPath + str(id) + ".SC2Replay", "wb").write(r.content)
 
 
 
