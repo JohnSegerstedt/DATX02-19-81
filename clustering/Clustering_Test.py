8583c6678354a4105dec3635ebd43cbe439c82b7
Arvid <arvid@wartenberg.se>
2019-02-25 11:40:39 +0100

    Changes to methods dude

--- clustering/Clustering_Test.py
+++ clustering/Clustering_Test.py
@@ -1,16 +1,18 @@
 from clustering.Utilities import Methods
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.metrics import pairwise_distances
-from sklearn.decomposition import SparsePCA,PCA
+
+
 
 df = pd.read_csv(r'../data/Replays2-210.0s.csv')
 df=df.drop('Unnamed: 0', axis=1)
-
 df = df.loc[:, (df != 0).any(axis=0)]
-Methods.heatMap(df)
-Methods.explainedVariance(df)
+
+
 df=Methods.clusterPCA(df,3)
 
-df_cluster=Methods.cluster_KMeans(df,3,True,True)
-Methods.project_onto_R3(df,['PCA 0','PCA 1','PCA 2'])
+df_cluster=Methods.cluster_KMeans(df,2,True)
+df_cluster=Methods.inversePCA(df_cluster,0.95)
+df_DBSCAN = Methods.cluster_DBSCAN(df_cluster,0.4,10,False,True)
+Methods.project_onto_R3(df_DBSCAN,[0,1,2])
+cluster_2 = df_DBSCAN.loc[df_DBSCAN['Names'] == 'Cluster 2']
+Methods.project_onto_R3(cluster_2,[0,1,2])
