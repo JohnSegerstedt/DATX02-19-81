98eceb03bd046c1eb6df1fec27155a218852cdd2
dafors <da_fors@hotmail.com>
2019-03-05 21:19:27 +0100

    Add functionality - move files that have been parsed into folder

--- sc2reaperHelper/reapHelper.py
+++ sc2reaperHelper/reapHelper.py
@@ -1,6 +1,7 @@
 import os
 import os.path
 import sys
+from sc2reaper import sc2reaper
 from datetime import datetime
 
 # Requests sc2reaper to parse all files in <folder>. 
@@ -8,14 +9,13 @@ from datetime import datetime
 # Steps per frame is managed in sc2reaper/sc2reaper/sweeper.py
 # Matchup specification (PvP or other), database name and port is managed in sc2reaper/sc2reaper/sc2reaper.py
 
-# To call this script:
-# $ python reapHelper.py <directory>
+# ----- INTERESTING VARS ------
+folder = "tmp10/" 	# The directory containing the replay files, change if needed.
+subdir = "parsed/" 	# Name of directory where rep files end up after parsing, no matter if parsing was successful nor not.
 
-# Input
-folder = str(sys.argv[1]) 
-
-# Vars
-dirPath = os.path.abspath(folder)
+# ----- SETUP ------
+dirToParse = os.path.abspath(folder)
+dirParsed = os.path.join(dirToParse, subdir)
 fileCounter = 0
 filesToBeProcessed = os.listdir(folder)
 totalNrFiles = len(filesToBeProcessed)
@@ -29,7 +29,7 @@ def parse(replayFile):
 	print("requesting parse of file number: ", fileCounter, " out of ", totalNrFiles, " ...")
 	print("total running time: ", (datetime.now() - startTime))
 	print(fileCounter)
-	fullPath = os.path.join(dirPath, replayFile)
+	fullPath = os.path.join(dirToParse, replayFile)
 	try:
 		#print("debug", fullPath)
 		os.system("sc2reaper ingest " + fullPath)
@@ -37,11 +37,22 @@ def parse(replayFile):
 	except:
 		print("Something went wrong with " + replayFile)
 
-# --- "MAIN" ---
+def moveFile(file):
+	origin = os.path.join(dirToParse, replayFile)
+	destination = os.path.join(dirParsed, replayFile)
+	os.rename(origin, destination)
+
+
+# ----- "MAIN" -----
 startTime = datetime.now()
+
+if not os.path.isdir(dirParsed):
+        os.makedirs(dirParsed)
 try:
 	for replayFile in filesToBeProcessed:
-		parse(replayFile)
+		if replayFile.endswith('.SC2Replay'):
+			parse(replayFile)
+			moveFile(replayFile)
 except:
 	print("Couldn't read directory")
 print("script finished")
