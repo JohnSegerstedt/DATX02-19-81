8b07486c90d248461ebddcb14f778070823a4a88
Arvid <arvid@wartenberg.se>
2019-03-12 10:02:00 +0100

    Changes to methods dude

--- clustering/Clustering_Test.py
+++ clustering/Clustering_Test.py
@@ -10,6 +10,42 @@ warnings.filterwarnings(action='ignore', category=DataConversionWarning) #Supres
 # For plotting the images
 from matplotlib import pyplot as plt
 
+def readAndPrep(dir):
+    df = pd.read_csv(dir)
+    del df['Unnamed: 0']
+    df = df.loc[:, (df!=0).any(axis=0)]
+    return df
+
+def clusterAtTimestamp(timestamp, ids):
+    dir = '../data/Replays6-' + str(timestamp) + 's.csv'
+    df = readAndPrep(dir)
+    df, nameCol = Methods.rmName(df)
+    df_km = Methods.cluster_KMeans(Methods.getPCs(df, 4), 3, True)
+
+    labels = df_km['Names']
+    dict = Methods.makeLabelDict(nameCol, labels)
+
+    dfs = []
+    for e in ids:
+        df_tmp = pd.read_csv('../data/Replays6-' + str(e) + 's.csv')
+        dfs.append(Methods.labelDf(dict, df_tmp))
+    return dfs
+
+def multicluster(ids):
+    i = 1
+    dfs_list = [clusterAtTimestamp(90, ids), clusterAtTimestamp(180, ids), clusterAtTimestamp(270, ids), clusterAtTimestamp(390, ids), clusterAtTimestamp(510, ids), clusterAtTimestamp(600, ids)]
+    for e in dfs_list:
+        plt.figure()
+        i = 1
+        for e2 in e:
+            #plt.figure()
+            plt.subplot(2, 3, i)
+            e2 = Methods.getPCs(e2, 3)
+            #Methods.project_onto_R3(e, ['PC 1', 'PC 2', 'PC 3'])
+            Methods.project_onto_R2(e2, ['PC 1', 'PC 2'], False)
+            i += 1
+    plt.show()
+    return dfs_list
 
 
 
