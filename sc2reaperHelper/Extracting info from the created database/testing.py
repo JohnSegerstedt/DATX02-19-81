f133be5b3f71f9d27ab2c0d0614f69f75ce693d9
dafors <da_fors@hotmail.com>
2019-03-13 09:48:28 +0100

    update to the script responsible for extracting data from the database. Still work in progress

--- sc2reaperHelper/Extracting info from the created database/testing.py
+++ sc2reaperHelper/Extracting info from the created database/testing.py
@@ -7,9 +7,9 @@ import datetime
 #Generates .CSV from connected MongoDB Server.
 
 # INTERESTING VARIABLES
-# frames come in sizes 12*x
-framesOfInterest = [12000] #1200,2400,3600]
-databaseName = "replay_database_test"
+# Choose which frames to extract data for. Frames come in sizes 12*x
+framesOfInterest = [11568] #1200,2400,3600]
+databaseName = "vision_testing"
 
 #Full game state: replay_id + frame_id + units1 + upgrades1 + units2 + upgrades2
 #One player game state: replay_id + frame_id + units1 + upgrades1 + (for each unit type; #max spotted in a single frame up to the current frame) 
@@ -115,51 +115,18 @@ class UnitId(enum.IntEnum):
   WarpPrismPhasing = 136
   Zealot = 73
 
-class rowIndex(enum.IntEnum):
-  Adept = 0
-  AdeptPhaseShift = 1
-  Archon = 2
-  Assimilator = 3
-  Carrier = 4
-  Colossus = 5
-  CyberneticsCore = 6
-  DarkShrine = 7
-  DarkTemplar = 8
-  Disruptor = 9
-  DisruptorPhased = 10
-  FleetBeacon = 11
-  ForceField = 12
-  Forge = 13
-  Gateway = 14
-  HighTemplar = 15
-  Immortal = 16
-  Interceptor = 17
-  Mothership = 18
-  MothershipCore = 19
-  Nexus = 20
-  Observer = 21
-  ObserverSurveillanceMode = 22
-  Oracle = 495
-  Phoenix = 78
-  PhotonCannon = 66
-  Probe = 84
-  Pylon = 60
-  PylonOvercharged = 894
-  RoboticsBay = 70
-  RoboticsFacility = 71
-  Sentry = 77
-  ShieldBattery = 1910
-  Stalker = 74
-  Stargate = 67
-  StasisTrap = 732
-  Tempest = 496
-  TemplarArchive = 68
-  TwilightCouncil = 65
-  VoidRay = 80
-  WarpGate = 133
-  WarpPrism = 81
-  WarpPrismPhasing = 136
-  Zealot = 73
+def extractSeenUnits(state):
+	stateData = {}
+
+	stateData["Replay_id"] = state["replay_id"]
+	stateData["Frame_id"] = state["frame_id"]
+	for unitKey in state["visible_enemy_units"].keys():
+		print()
+		unitType = UnitId(int(unitKey)).name + str(state["player_id"])
+		stateData[unitType] = len(state["visible_enemy_units"][unitKey])
+	    #print(unitType)
+	    #print(len(state["units"][unitKey]))
+	return stateData
 
 def extractData(state):
   global dictUpgradeIndex
@@ -183,12 +150,17 @@ def extractData(state):
   return stateData
 
 def saveToDataframe(data, playerId):
-  global df_test
   global df_p1
   global df_p2
   global df
   df = df.append(data, ignore_index=True)
-  df_test = df_test.append(data, ignore_index=True)
+
+  if playerId == 1:
+  	print(playerId)
+  	df_p1 = df_p1.append(data, ignore_index=True)
+  else:
+  	print(playerId)
+  	df_p2 = df_p2.append(data, ignore_index=True)
 
 def printCSV(dataFrame):
   #file = str(destination)
@@ -225,40 +197,53 @@ p2_units_upgrades = [entry + "2" for entry in (units + upgrades)]
 df_p1 = pd.DataFrame(columns=extra + p1_units_upgrades) #dtype='int'
 df_p2 = pd.DataFrame(columns=extra + p2_units_upgrades)
 df = pd.DataFrame(columns=extra + p1_units_upgrades + p2_units_upgrades)
-#with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-#    print(df_p1)
-df_test = pd.DataFrame()
 
-# INITIATING SORTING TUPLES
-dictUpgradeIndex = {}
-i = 0
-for e in UpgradeId:
-  dictUpgradeIndex[e.value] = i 
-  i+=1
 
-dictUnitIndex = {}
-i = 0
-for e in UnitId:
-  dictUnitIndex[e.value] = i 
-  i+=1
-
-# TODO generalize for all games, and for each state of interest
+# TODO generalize for all played games, and for each state of interest for each player
   # TODO iterate through each frame, count spotted units of each type, keeping track of max so far, for one game up until state of interest
   # states_visible_enemy_units = []
 
-states = db.states.aggregate([
+
+# RETRIEVE STATE DATA FROM MONGO
+stateData = db.states.aggregate([
   #get matching stuff
     { "$match" : {"frame_id" : {"$in" : framesOfInterest }, } },
-  #define what to expose from above match.
+  #define what to expose from the matching above.
     {"$project" : {"replay_id": 1, "_id" : 0, "frame_id" : 1, "player_id" : 1, "units" : 1, "upgrades" : 1 }},
     ])
 
+# PARSE STATE DATA
 count = 0
-for state in states:
+for state in stateData:
   newRow = extractData(state)
   saveToDataframe(newRow, state["player_id"])
   count += 1
   #print("States saved:", count)
+
+# RETRIEVE VISIBLE ENEMY UNITS DATA FROM MONGO
+replay_ids = []
+for replay_doc in replays.find({}, {"replay_id": 1}):
+	replay_ids.append(replay_doc["replay_id"])
+
+for replay_id in replay_ids:
+	stateData = db.states.aggregate([
+  #get matching stuff
+	    { "$match" : {"replay_id" : replay_id, "player_id" : 1} },
+	  #define what to expose from the matching above.
+	    {"$project" : {"replay_id": 1, "_id" : 0, "frame_id" : 1, "player_id" : 1, "visible_enemy_units" : 1 }},
+	    {"$sort" : { "frame_id" : 1} }
+	    ])
+
+	for state in stateData:
+		print("state: ", state["replay_id"], "frame: ", state["frame_id"])
+		seenUnits = extractSeenUnits(state)
+		print(seenUnits)
+
+
+
+# PARSE VISIBLE ENEMY UNITS DATA
+#for replay_doc in state.find({}, {"replay_id": 1}):
+#	print(replay_doc)
 
 print("Finished, total states parsed:", count)
 
@@ -266,8 +251,22 @@ print("FINAL OUTPUT test")
 #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 #    print(df_test)
 
-printCSV(df)
 
-#pprint.pprint(df)
+
+pprint.pprint(df)
+
+
+#TODO only one players stuff on each currently? Make two separate and merge p1 p2 like: 
+print("DFP!")
+print(df_p1)
+print(df_p2)
+#print(df_p1.columns.difference(df_p2.columns))
+
+#Merge player 1 and player 2 data into one state. 
+print(pd.merge(df_p1,df_p2, on=['Replay_id', 'Frame_id'], how = "outer"))
+#https://pythonprogramming.net/join-merge-data-analysis-python-pandas-tutorial/
+
+#TODO comopare columns. Rename to make them fit?
+#TODO remove df throughout program
 #TODO remove NaNs
-print(list(df_test.columns.values))
+printCSV(df)
