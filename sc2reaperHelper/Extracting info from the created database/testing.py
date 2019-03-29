e159a40a03dcf3f51d4224856c9469e7558895e9
dafors <da_fors@hotmail.com>
2019-03-29 15:35:15 +0100

    update mongodb to csv script, add result

--- sc2reaperHelper/Extracting info from the created database/testing.py
+++ sc2reaperHelper/Extracting info from the created database/testing.py
@@ -20,6 +20,8 @@ printResultShort = False
 
 parseFlag = 1 # 0 = all data, 1 = cluster data, 2 = training data
 
+allColumns = ['0Frame_id', '0P1_mmr', '0P1_result', '0P2_mmr','0P2_result', '0Replay_id', 'P1_Supply_army', 'P1_Supply_total', 'P1_Supply_used', 'P1_Supply_workers', 'P1_Unit_Adept', 'P1_Unit_AdeptPhaseShift', 'P1_Unit_Archon', 'P1_Unit_Assimilator', 'P1_Unit_Carrier', 'P1_Unit_Colossus', 'P1_Unit_CyberneticsCore', 'P1_Unit_DarkShrine', 'P1_Unit_DarkTemplar', 'P1_Unit_Disruptor', 'P1_Unit_DisruptorPhased', 'P1_Unit_FleetBeacon', 'P1_Unit_Forge', 'P1_Unit_Gateway', 'P1_Unit_HighTemplar', 'P1_Unit_Immortal', 'P1_Unit_Interceptor', 'P1_Unit_Mothership', 'P1_Unit_Nexus', 'P1_Unit_Observer', 'P1_Unit_ObserverSurveillanceMode', 'P1_Unit_Oracle', 'P1_Unit_Phoenix', 'P1_Unit_PhotonCannon', 'P1_Unit_Probe', 'P1_Unit_Pylon', 'P1_Unit_RoboticsBay', 'P1_Unit_RoboticsFacility', 'P1_Unit_Sentry', 'P1_Unit_ShieldBattery', 'P1_Unit_Stalker', 'P1_Unit_Stargate', 'P1_Unit_StasisTrap', 'P1_Unit_Tempest', 'P1_Unit_TemplarArchive', 'P1_Unit_TwilightCouncil', 'P1_Unit_VoidRay', 'P1_Unit_WarpGate', 'P1_Unit_WarpPrism', 'P1_Unit_WarpPrismPhasing', 'P1_Unit_Zealot', 'P1_Upgrade_AdeptPiercingAttack', 'P1_Upgrade_BlinkTech', 'P1_Upgrade_CARRIERLAUNCHSPEEDUPGRADE', 'P1_Upgrade_Charge', 'P1_Upgrade_DarkTemplarBlinkUpgrade', 'P1_Upgrade_ExtendedThermalLance', 'P1_Upgrade_GraviticDrive', 'P1_Upgrade_ObserverGraviticBooster', 'P1_Upgrade_PhoenixRangeUpgrade', 'P1_Upgrade_ProtossAirArmorsLevel1', 'P1_Upgrade_ProtossAirArmorsLevel2', 'P1_Upgrade_ProtossAirArmorsLevel3', 'P1_Upgrade_ProtossAirWeaponsLevel1', 'P1_Upgrade_ProtossAirWeaponsLevel2', 'P1_Upgrade_ProtossAirWeaponsLevel3', 'P1_Upgrade_ProtossGroundArmorsLevel1', 'P1_Upgrade_ProtossGroundArmorsLevel2', 'P1_Upgrade_ProtossGroundArmorsLevel3', 'P1_Upgrade_ProtossGroundWeaponsLevel1', 'P1_Upgrade_ProtossGroundWeaponsLevel2', 'P1_Upgrade_ProtossGroundWeaponsLevel3', 'P1_Upgrade_ProtossShieldsLevel1', 'P1_Upgrade_ProtossShieldsLevel2', 'P1_Upgrade_ProtossShieldsLevel3', 'P1_Upgrade_PsiStormTech', 'P1_Upgrade_WarpGateResearch', 'P1_minerals', 'P1_vespene', 'P2_Supply_army', 'P2_Supply_total', 'P2_Supply_used', 'P2_Supply_workers', 'P2_Unit_Adept', 'P2_Unit_AdeptPhaseShift', 'P2_Unit_Archon', 'P2_Unit_Assimilator', 'P2_Unit_Carrier', 'P2_Unit_Colossus', 'P2_Unit_CyberneticsCore', 'P2_Unit_DarkShrine', 'P2_Unit_DarkTemplar', 'P2_Unit_Disruptor', 'P2_Unit_DisruptorPhased', 'P2_Unit_FleetBeacon', 'P2_Unit_Forge', 'P2_Unit_Gateway', 'P2_Unit_HighTemplar', 'P2_Unit_Immortal', 'P2_Unit_Interceptor', 'P2_Unit_Mothership', 'P2_Unit_Nexus', 'P2_Unit_Observer', 'P2_Unit_ObserverSurveillanceMode', 'P2_Unit_Oracle', 'P2_Unit_Phoenix', 'P2_Unit_PhotonCannon', 'P2_Unit_Probe', 'P2_Unit_Pylon', 'P2_Unit_RoboticsBay', 'P2_Unit_RoboticsFacility', 'P2_Unit_Sentry', 'P2_Unit_ShieldBattery', 'P2_Unit_Stalker', 'P2_Unit_Stargate', 'P2_Unit_StasisTrap', 'P2_Unit_Tempest', 'P2_Unit_TemplarArchive', 'P2_Unit_TwilightCouncil', 'P2_Unit_VoidRay', 'P2_Unit_WarpGate', 'P2_Unit_WarpPrism', 'P2_Unit_WarpPrismPhasing', 'P2_Unit_Zealot', 'P2_Upgrade_AdeptPiercingAttack', 'P2_Upgrade_BlinkTech', 'P2_Upgrade_CARRIERLAUNCHSPEEDUPGRADE', 'P2_Upgrade_Charge', 'P2_Upgrade_DarkTemplarBlinkUpgrade', 'P2_Upgrade_ExtendedThermalLance', 'P2_Upgrade_GraviticDrive', 'P2_Upgrade_ObserverGraviticBooster', 'P2_Upgrade_PhoenixRangeUpgrade', 'P2_Upgrade_ProtossAirArmorsLevel1', 'P2_Upgrade_ProtossAirArmorsLevel2', 'P2_Upgrade_ProtossAirArmorsLevel3', 'P2_Upgrade_ProtossAirWeaponsLevel1', 'P2_Upgrade_ProtossAirWeaponsLevel2', 'P2_Upgrade_ProtossAirWeaponsLevel3', 'P2_Upgrade_ProtossGroundArmorsLevel1', 'P2_Upgrade_ProtossGroundArmorsLevel2', 'P2_Upgrade_ProtossGroundArmorsLevel3', 'P2_Upgrade_ProtossGroundWeaponsLevel1', 'P2_Upgrade_ProtossGroundWeaponsLevel2', 'P2_Upgrade_ProtossGroundWeaponsLevel3', 'P2_Upgrade_ProtossShieldsLevel1', 'P2_Upgrade_ProtossShieldsLevel2', 'P2_Upgrade_ProtossShieldsLevel3', 'P2_Upgrade_PsiStormTech', 'P2_Upgrade_WarpGateResearch', 'P2_minerals', 'P2_vespene']
+
 class UpgradeId(enum.Enum):
   ProtossAirArmorsLevel1 = 81
   ProtossAirArmorsLevel2 = 82
@@ -170,16 +172,16 @@ def saveAsCSV(dataFrame, appendName, playerId):
   dataFrame.to_csv("csvs/" + str(fileName) + str(appendName) + str(playerString) + ".csv")
 
 
-def getMmr(replayId, playerId, database):
+def getMmrAndResult(replayId, playerId, database):
 	playerData = database.players.aggregate([
 	#get matching stuff
 	{ "$match" : {"replay_id" : str(replayId), "player_id" : playerId} },
 	#define what to expose from the matching above.
-    {"$project" : {"player_mmr" : 1, "player_id" : 1}},
+    {"$project" : {"player_mmr" : 1, "player_id" : 1, "result" : 1}},
     ])
 
 	for match in playerData:
-		return match["player_mmr"]
+		return match["player_mmr"], match["result"]
 
 
 def connectToDatabase(db):
@@ -206,9 +208,12 @@ def printInfoShort(dataFrame):
 	print("Number of rows, columns: ", dataFrame.shape)
 
 def extractFrameState(frameId, replayIds):
+	global allColumns
 	# INITIATING DATA FRAMES
+	#df_p1 = pd.DataFrame(columns=allColumns) #dtype='int'
 	df_p1 = pd.DataFrame(columns=["0Replay_id", "0Frame_id"]) #dtype='int'
 	df_p2 = pd.DataFrame(columns=["0Replay_id", "0Frame_id"]) #dtype='int'
+	#df_p2 = pd.DataFrame(columns=allColumns) #dtype='int'
 	stateData = db.states.aggregate([
 		  #get matching stuff
 		    #{ "$match" : {"frame_id" : {"$in" : framesOfInterest }, } },
@@ -226,9 +231,10 @@ def extractFrameState(frameId, replayIds):
 	  	countRejected += 1
 	  else:
 	  	newRow = extractData(state)
-	  	#get mmr
-	  	mmr = getMmr(state["replay_id"], state["player_id"], db)
+	  	#get mmr and result
+	  	mmr, result = getMmrAndResult(state["replay_id"], state["player_id"], db)
 	  	newRow["0P" + str(state["player_id"]) + "_mmr"] = mmr
+	  	newRow["0P" + str(state["player_id"]) + "_result"] = result
 	  	#saveToDataframe(newRow, state["player_id"], df_p1, df_p2)
 	  	if state["player_id"] == 1:
 	  		df_p1 = df_p1.append(newRow, ignore_index=True)
@@ -252,6 +258,9 @@ def extractFrameState(frameId, replayIds):
 	else: 
 		#Merge player 1 and player 2 data into one joint dataframe. 
 		merged = pd.merge(df_p1,df_p2, on=['0Replay_id', '0Frame_id'], how = "outer")
+		
+		#add all columns
+		merged = merged.reindex_axis(allColumns, 'columns')
 
 		if printResult:
 			printInfo(merged)
